8fef0ef3a849dee4745f5fe412b372e8f439169a
Arona Coker <a.coker130@gmail.com>
2026-09-23 10:24:26 +0200

    Created: Grenzen

---
+++ src/Grenzen.java
@@ -0,0 +1,26 @@
+public class Grenzen {
+    static void main() {
+
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MAX_VALUE + 1);
+
+
+       long ageInSeconds = (long)70 * 365 * 24 * 60 * 60;
+        System.out.println(ageInSeconds);
+
+
+    double compareNumber1 = 0.1;
+    double compareNumber2 = 0.2;
+    double compareNumber3 = 0.3;
+        System.out.println(compareNumber1 + compareNumber2);
+        System.out.println((compareNumber1 + compareNumber2) == compareNumber3);
+
+
+        int compareNumber4 = 1;
+        int compareNumber5 = 2;
+        int compareNumber6 = 3;
+
+        System.out.println(compareNumber4 + compareNumber5 == compareNumber6);
+
+    }
+}
